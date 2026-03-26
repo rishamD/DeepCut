1265968709d315791381ba1342fb8370b16c7fad
Rishamdeep Singh <rishamdeep44@gmail.com>
2026-03-26 18:39:32 -0500

    new stuff

--- server/app.py
+++ server/app.py
@@ -1,12 +1,20 @@
-# app.py
+import os, pymysql
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
+
+db = pymysql.connect(
+    host=os.getenv("DB_HOST"),
+    port=int(os.getenv("DB_PORT")),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    cursorclass=pymysql.cursors.DictCursor
+)
 
 @app.route("/api/suggest", methods=["POST"])
 def suggest():
-    username = request.json.get("letterboxdUser")
-    # TODO: query DB & return movie list
-    return jsonify({"movies": ["Parasite", "Everything Everywhere All at Once"]})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    with db.cursor() as cur:
+        cur.execute("SELECT 1 as db_ok")
+        row = cur.fetchone()
+    return jsonify({"movies": ["Parasite", "Everything Everywhere All at Once"], "db_ok": row["db_ok"]})
